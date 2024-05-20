ad4ad1af17c9dc2d5bdb7b8533949e868fc1ef5c
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2024-05-21 00:44:58 +0700

    issue 1

--- app/View/Components/adminLayout.php
+++ app/View/Components/adminLayout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class AdminLayout extends Component
+class adminlayout extends Component
 {
     /**
      * Create a new component instance.
